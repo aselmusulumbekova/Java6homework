bcf20d69bb9b03b1134038c241a08406b55dfe2e
Асель <musulumbekovaasel@gmail.com>
2022-07-12 21:06:06 +0600

    Initial commit Home work 3

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("     J      a      v     v      a");
+        System.out.println("    JJ     a a      v   v      a a");
+        System.out.println(" J  JJ    aaaaa      v v      aaaaa");
+        System.out.println("  JJ      a   a       v       a   a");
+
+    }
+}
